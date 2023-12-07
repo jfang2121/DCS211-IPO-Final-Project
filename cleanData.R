3f9f5a4a10072dbd0c41ebe4583fadb763e0cddc
Ian Mayral Boyle <imayralb@bates.edu>
2023-12-07 12:24:53 -0500

    More Data Cleaning

--- cleanData.R
+++ cleanData.R
@@ -12,8 +12,24 @@ View(ipo_raw)
 ipo <- ipo_raw %>% 
   filter(Country.Region.Full.Name == "UNITED STATES") %>% 
   filter(stringr::str_detect(Offer.Type, "IPO")) %>% 
-  filter(Offer.To.1st.6.Months != "" & Offer.To.1st.Year != "")
-  # Rename columns
+  filter(Offer.To.1st.Close != ""
+         & Offer.To.Month.1 != ""
+         & Offer.To.1st.6.Months != ""
+         & Offer.To.1st.Year != "") %>% 
+  rename(date = Effective.Date, 
+         first_day = Offer.To.1st.Close, 
+         first_month = Offer.To.Month.1, 
+         first_6m = Offer.To.1st.6.Months, 
+         first_year = Offer.To.1st.Year) %>% 
+  select(date, first_day, first_month, first_6m, first_year) %>% 
+  mutate(first_day = as.numeric(first_day), 
+         first_month = as.numeric(first_month), 
+         first_6m = as.numeric(first_6m), 
+         first_year = as.numeric(first_year)) %>% 
+  mutate(pop_day = ifelse(first_day > 0, 1, 0), 
+         pop_month = ifelse(first_month > 0, 1, 0), 
+         pop_6m = ifelse(first_6m > 0, 1, 0), 
+         pop_year = ifelse(first_year > 0, 1, 0))
 View(ipo)
 
 sp <- sp_raw %>% 
